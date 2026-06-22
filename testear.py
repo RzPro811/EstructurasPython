08f6fcbcb26cfd00736509326cd8fc7678a25381
joelz <joelzarram@gmail.com>
2026-06-22 16:37:26 -0300

    algo

--- testear.py
+++ testear.py
@@ -1,6 +1,13 @@
-from src.Estructuras.Grafo import Vertice, Arista
+from src.Estructuras.Grafo import Grafo
 from src.Estructuras.Vector import validarTipoObjeto
 
-Vertice(str, "string")
 
-Arista(None)
+grafo = Grafo(int,str)
+
+grafo.agregarVertice(1)
+grafo.agregarVertice(2)
+grafo.agregarVertice(3)
+
+grafo.conectarVertices(0,1,"Hola")
+
+print(grafo)

f30090d719f1918185d53aa9e8fb63318437620e
joelz <joelzarram@gmail.com>
2026-06-24 22:12:10 -0300

    Reparaciones en el Heap y datos movidos

--- testear.py
+++ testear.py
@@ -1,14 +1,17 @@
-from src.Estructuras.Vector import Heap, Vector
+from src.Estructuras.Vector import Vector
+from src.Estructuras.NoLineales import Heap
 from random import randint
 
 heap  = Heap(int)
+
+print(heap.estaVacio())
 
 for i in range(10):
     numero = randint(0,100)
     print(numero)
     heap.agregar(numero)
 
-vector = Vector(int, heap.getCantidadElementos())
+vector = Vector(int,10)
 
 while not heap.estaVacio():
     print(vector)

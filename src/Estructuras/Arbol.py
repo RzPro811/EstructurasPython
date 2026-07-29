a5be4ffe2e180a7f0e4e17f9de1cdc53757aa437
joelz <joelzarram@gmail.com>
2026-07-28 23:21:35 -0300

    Cosas Basicas

--- src/Estructuras/Arbol.py
+++ src/Estructuras/Arbol.py
@@ -1,44 +1,96 @@
-from Heredables import TypeStruct, DataStruct, Generic, T
-from Validaciones import validarTipoObjeto
+from .Heredables import TypeStruct, DataStruct, Generic, T
+from .Validaciones import validarTipoObjeto, validarCondicion
 
 #SEMILLA -------------------------------------------------------------------------------------------------------------
 class SemillaBin(DataStruct, Generic[T]):
+    """# SEMILLA BINARIA
+    
+    Hecha para crear arboles binarios
+    No hay mucho misterio
+    """
     #ATRIBUTOS
     __izquierda:SemillaBin[T]
     __derecha:SemillaBin[T]
 
     #CONSTRUCTOR
-    def __init__(self, tipo, dato):
+    def __init__(self, tipo:type, dato:T):
+        """Dado un tipo de dato y un dato, crea una semilla binaria sin hijos
+        
+        **parameters**
+            -   tipo (type)
+            -   dato (T)
+
+        **excepciones**
+            -   **TypeError** si el parametro del tipo T no es type o el dato ingresado no es del tipo ingresado T
+        """
         super().__init__(tipo, dato, False)
         self.setIzquierda(None)
         self.setDerecha(None)
 
     #METODOS DE CLASE
     def tieneHijoIzq(self) -> bool:
+        """Verifica que la semilla tenga hijo izquierdo
+        
+        **return**
+            -   (bool) Verdadero si el hijo izquierdo no es None, falso si sí
+        """
         return self.getIzquierda() is not None
 
     def tieneHijoDer(self) -> bool:
+        """Verifica que la semilla tenga hijo derecho
+        
+        **return**
+            -   (bool) Verdadero si el hijo derecho no es None, falso si sí
+        """
         return self.getDerecha() is not None
 
     def tieneHijos(self) -> bool:
+        """Verifica que la semilla tenga hijos
+        
+        **return**
+            -   (bool) Verdadero si la semilla tiene al menos un hijo, falso si no tiene ninguno
+        """
         return self.tieneHijoDer() or self.tieneHijoIzq()
 
     def tieneAmbosHijos(self) -> bool:
+        """Verifica que la semilla tenga ambos hijos
+        
+        **return**
+            -   (bool) Verdadero si tiene los dos hijos, falso si no tiene al menos uno
+        """
         return self.tieneHijoIzq() and self.tieneHijoDer()
 
     def desconectar(self):
+        """Desconecta la semillas hijas de esta semilla"""
         self.setIzquierda(None)
         self.setDerecha(None)
 
     #VALIDACIONES
     @staticmethod
     def validarSemilla(semilla:SemillaBin):
+        """Valida que una semilla sea una semilla
+        
+        **parameters**
+            -   semilla (SemillaBin): puede ser None
+
+        **excepciones**
+            -   **TypeError**: si el parametro ingresado no es una semiila**
+        """
         if semilla is not None:
             validarTipoObjeto(SemillaBin, semilla, "Ingresa una semilla")
 
+    @staticmethod
+    def validarTiposSemilla(semilla1:SemillaBin, semilla2:SemillaBin):
+        SemillaBin.validarSemilla(semilla1)
+        SemillaBin.validarSemilla(semilla2)
+        validarCondicion(issubclass(semilla1.getType(), semilla2.getType()) or issubclass(semilla2.getType(), semilla1.getType()),
+                         "Ingresa dos semillas con el mismo tipo de Dato", TypeError)
+        
     #METODOS ESTATICOS
     @staticmethod
     def intercambiarDatos(semilla1:SemillaBin, semilla2:SemillaBin):
+        SemillaBin.validarTiposSemilla(semilla1, semilla2)
+
         dato = semilla1.getDato()
         semilla1.setDato(semilla2.getDato())
         semilla2.setDato(dato)
@@ -77,18 +129,68 @@ class ArbolBin(Generic[T], TypeStruct):
     #ATRIBUTOS
     __raiz:SemillaBin[T]
     __ordenamiento:function
+    __cantidadElementos:int
 
     #CONSTRUCTOR
-    def __init__(self, tipo, metodo:function = None):
+    def __init__(self, tipo:type, metodo:function = None):
         super().__init__(tipo)
 
-        self.__ordenamiento = metodo
-        self.__raiz = None
+        self.__setMetodo(metodo)
+        self.__setRaiz(None)
+        self.__iniciarConteo()
 
+    #METODOS DE CLASE
+    def estaEquilibrado(self) -> bool:
+        return self.__getRaiz().getEquilibrio() == 0
+
+    def estaVacio(self) -> bool:
+        return self.getCantidadElementos() == 0
+
+    def agregar(self, item:T):
+        self.__validarEntrada__(item)
+        self.__agregarItem()
+
+    def remover(self, item:T):
+        self.__validarEntrada__(item)
+        self.__quitarItem()
+
+    def mapear(self) -> list:pass 
+
+    #METODOS INTERNOS
+    def __iniciarConteo(self):
+        self.__cantidadElementos = 0
+
+    def __agregarItem(self):
+        self.__cantidadElementos +=1
+
+    def __quitarItem(self):
+        self.__cantidadElementos -=1
+
+    def __estaMasOMenosEquilibrado(self) -> bool:
+        return self.__getRaiz().getEquilibrio() in (-1, 0, 1)
+
+    def __hayMetodo(self) -> bool:
+        return self.__ordenamiento is not None
+
+    def __menorIgual(self, item1:T, item2:T) -> bool:
+        if self.__hayMetodo(): return self.__metodo(item1) <= self.__metodo(item2)
+        return item1 <= item2
+
+    def __metodo(self, entrada:T) -> object:
+        return self.__ordenamiento(entrada)
 
     #GETTERS
+    def __getRaiz(self) -> SemillaBin:
+        return self.__raiz
+
     def getCantidadElementos(self) -> int:
-        return 0
+        return self.__cantidadElementos
     
     #SETTERS
-    
+    def __setRaiz(self, raiz:SemillaBin):
+        if SemillaBin is not None: SemillaBin.validarSemilla(raiz)
+        self.__raiz = raiz
+
+    def __setMetodo(self, metodo:function):
+        if metodo is not None:validarCondicion(callable(metodo))
+        self.__ordenamiento = metodo        
